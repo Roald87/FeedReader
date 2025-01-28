174684c65e1ecab3b11be7b0cf49832ba7a95e33
Roald Ruiter <roaldruiter@gmail.com>
2025-01-28 21:48:05 +0100

    add atom03 support

---
+++ FeedReader/Feeds/Atom03/Atom03Feed.cs
@@ -0,0 +1,84 @@
+namespace Roald87.FeedReader.Feeds
+{
+    using System;
+    using System.Xml.Linq;
+
+    public class Atom03Feed : BaseFeed
+    {
+        /// <summary>
+        /// The "author" element
+        /// </summary>
+        public AtomPerson Author { get; set; }
+
+        /// <summary>
+        /// The "modified" element as string
+        /// </summary>
+        public string ModifiedString { get; set; }
+
+        /// <summary>
+        /// The "updated" element as DateTime. Null if parsing failed of modifiedString is empty.
+        /// </summary>
+        public DateTime? Modified { get; set; }
+
+        /// <summary>
+        /// The "generator" element
+        /// </summary>
+        public string Generator { get; set; }
+        
+        /// <summary>
+        /// The "id" element
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The "tagline" element
+        /// </summary>
+        public string Tagline { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Atom03Feed"/> class.
+        /// default constructor (for serialization)
+        /// </summary>
+        public Atom03Feed()
+            : base()
+        {
+        }
+
+        public Atom03Feed(string feedXml, XElement feed)
+            : base(feedXml, feed)
+        {
+            this.Link = feed.GetElement("link")?.Attribute("href")?.Value;
+            this.Author = new AtomPerson(feed.GetElement("author"));
+            this.Tagline = feed.GetElement("tagline")?.Value;
+            this.Id = feed.GetValue("id");
+            this.ModifiedString = feed.GetElement("modified")?.Value;
+            this.Modified = Helpers.TryParseDateTime(this.ModifiedString);
+            this.Generator = feed.GetElement("generator")?.Value;
+
+            var items = feed.GetElements("entry");
+            foreach (var item in items)
+            {
+                this.Items.Add(new Atom03FeedItem(item));
+            }
+        }
+
+        /// <summary>
+        /// Creates the base <see cref="Feed"/> element out of this feed.
+        /// </summary>
+        /// <returns>feed</returns>
+        public override Feed ToFeed()
+        {
+            Feed f = new Feed(this)
+            {
+                Copyright = null,
+                Description = null,
+                ImageUrl = null,
+                Language = null,
+                LastUpdatedDate = this.Modified,
+                LastUpdatedDateString = this.ModifiedString,
+                Type = FeedType.Atom03
+            };
+            return f;
+        }
+    }
+}
---
+++ FeedReader/Parser/Atom03Parser.cs
@@ -0,0 +1,14 @@
+﻿namespace Roald87.FeedReader.Parser
+{
+    using System.Xml.Linq;
+    using Feeds;
+
+    internal class Atom03Parser : AbstractXmlFeedParser
+    {
+        public override BaseFeed Parse(string feedXml, XDocument feedDoc)
+        {
+            Atom03Feed feed = new Atom03Feed(feedXml, feedDoc.Root);
+            return feed;
+        }
+    }
+}
